38e4d842adf92d2d390561eb3ef594253264bd6f
Imad Ibrahim <ibrahim.imad5@gmail.com>
2013-03-06 00:27:19 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+	def home
+	end
 
-  def about
-  end
+	def about
+	end
 
-  def help
-  end
+	def help
+	end
+
+	def contact
+	end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleAppV3::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/about"
+  root to: 'static_pages#home'
+  
+  match '/', to: 'static_pages#home'
+  match '/about', to: 'static_pages#about'
+  match '/help', to: 'static_pages#help'
+  match '/contact', to: 'static_pages#contact'
 
-  get "static_pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
