5768adc7078225ffdfcbea1398add7e4fa991540
vagrant <vagrant@rails.local.dev>
2013-03-13 20:22:12 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 SampleAppV3::Application.routes.draw do
 
-  get "users/new"
-
+  resources :users
+  
   root to: 'static_pages#home'
   
   match '/', to: 'static_pages#home'
+  match '/signup', to: 'users#new'
   match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
